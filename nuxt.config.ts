3787d2a83b69e532549b8dd23e78a87c2c73d274
Tayna <tayna@espressolabs.com.br>
2023-12-13 22:14:59 -0300

    Package: adicionado pinia para gerenciar estados

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook', '@nuxtjs/tailwindcss'],
-  
+  modules: ['@nuxtjs/storybook', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 })
