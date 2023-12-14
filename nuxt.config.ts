afece567907ff92e0780415f5886fdb13fe580ab
Tayna <tayna@espressolabs.com.br>
2023-12-14 08:17:11 -0300

    nuxt.config: Adicionado modulo do google fonts e configurações de css no css/main

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: [
+    '@nuxtjs/storybook',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/google-fonts',
+  ],
+  googleFonts: {
+    families: {
+      'Open Sans': true,
+      Righteous: true,
+    },
+    display: 'swap',
+  },
   pinia: {
     storesDirs: ['./stores/**'],
   },
-})
+  css: ['~/assets/css/main.css'],
+});
