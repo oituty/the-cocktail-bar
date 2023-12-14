6bb7afc336401947ceb917c5a5992140a6fd20bc
Tayna <tayna@espressolabs.com.br>
2023-12-14 08:15:56 -0300

    app.vue: adicionado NuxtPage para renderizar rotas nas pastas /pages

--- app.vue
+++ app.vue
@@ -1,16 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-    <div class="favorites">
-      <div v-for="favorite in favoritesList" :key="favorite" class="favorite">
-        {{ favorite }}
-        <button @click="removeFromFavorites(favorite)">Fechar</button>
-      </div>
-    </div>
-    <button @click="addToFavorites((favoritesList.length + 1).toString())">
-      Adicionar favorito
-    </button>
-  </div>
+  <NuxtPage />
 </template>
 
 <script setup lang="ts">
@@ -35,34 +24,3 @@ const removeFromFavorites = (cocktailId: string) => {
 const favoritesList = computed(() => favoritesStore.listFavorites());
 
 </script>
-<style>
-.favorites {
-  position: fixed;
-  top: 0;
-  right: 0;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-}
-
-.favorite {
-  background-color: #f0f0f0;
-  padding: 8px;
-  margin-top: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-button {
-  background-color: #ff0000;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  padding: 4px 8px;
-  cursor: pointer;
-}
-</style>
