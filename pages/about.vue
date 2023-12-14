846f61de7592f1f197af24d5a43d0f81cf92948a
Tayna <tayna@espressolabs.com.br>
2023-12-14 16:18:33 -0300

    base das telas de cocktails e favorites

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Sobre Nós</h1>
-    <p>Esta é a página sobre nós.</p>
-    <nuxt-link to="/">Voltar para a Página Inicial</nuxt-link>
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-primary font-bold underline">Hello world!</h1>
-    <h2 class="font-secondary">Página Inicial</h2>
-    <p class="font-tertiary">Bem-vindo ao nosso site!</p>
-    <nuxt-link to="/about">Sobre Nós</nuxt-link>
-  </div>
+  
 </template>
+
+<script setup lang="ts">
+
+</script>
