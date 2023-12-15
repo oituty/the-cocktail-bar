024e881ad734ffe37830dce8e07131afa3753320
Tayna <tayna@espressolabs.com.br>
2023-12-15 09:50:21 -0300

    layouts/defaut: ajustes no layout default do projeto

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,18 @@
   <div class="flex flex-col h-screen overflow-hidden">
     <Navbar />
     <div class="flex flex-1 overflow-hidden">
-      <div class="grid relative md:grid-cols-2 h-full w-full">
-        <Sidebar class="hidden md:flex" />
-        <transition name="slide">
-          <ClosedBarBanner class="absolute z-10 w-full md:w-2/4 right-0 h-full" v-show="!isBarOpened" />
-        </transition>
-        <transition name="page">
-          <main v-if="isBarOpened" class="overflow-auto">
+      <transition name="page" v-if="isBarOpened">
+        <main class="flex flex-1 overflow-hidden">
           <slot />
         </main>
-        </transition>
-      </div>
+      </transition>
+
+      <transition name="slide">
+        <ClosedBarBanner
+          class="absolute z-10 w-full right-0 h-full"
+          v-show="!isBarOpened"
+        />
+      </transition>
     </div>
   </div>
 </template>
@@ -24,17 +25,18 @@ const isBarOpened = computed(() => barStore.isBarOpen);
 </script>
 
 <style>
-
-.slide-enter-active, .slide-leave-active {
+.slide-enter-active,
+.slide-leave-active {
   transition: transform 0.5s;
 }
 
-.slide-enter-from, .slide-leave-to {
-  transform: translateX(98%);
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(100%);
 }
 
-.slide-enter-to, .slide-leave-from {
+.slide-enter-to,
+.slide-leave-from {
   transform: translateX(0);
 }
-
 </style>
